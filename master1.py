b399baec9f5bb51aa4b71bfec95221c2fbefdd9f
Yang Hyeon Jun <yang960920@naver.com>
2023-02-28 10:44:58 +0900

    MasterCommit 2
    빼기 함수 기능 추가

--- master1.py
+++ master1.py
@@ -5,6 +5,14 @@ def add_func(n1,n2) :
     res = n1 + n2
     return res
 
+def minus_func(n1,n2) :
+    res = n1 - n2
+    return res
+
 result = add_func(num1,num2)
 
 print(num1,'+',num2,'=',result)
+
+result = minus_func(num1,num2)
+
+print(num1,'-',num2,'=',result)
